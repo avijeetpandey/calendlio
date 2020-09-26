55ff552669c1cfba5c74a6cc0d1c289fea88f0b8
Avijeet Pandey <avijeetpandey25@gmail.com>
2020-09-26 22:01:39 +0530

    Added url strings

---
+++ lib/utils/strings.dart
@@ -0,0 +1,9 @@
+class Strings {
+  static final registerUrl =
+      'https://calendlio.sarayulabs.com/api/auth/register';
+  static final loginUrl = 'https://calendlio.sarayulabs.com/api/auth/login';
+  static final createMeetingUrl =
+      'https://calendlio.sarayulabs.com/api/bookings';
+  static final getAllMeetingsUrl =
+      'https://calendlio.sarayulabs.com/api/bookings';
+}
